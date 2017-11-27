44c5823047407ce6be29b4728fa79d8e9b8bfedc
Sulabh Shrestha <sulabh.shr@gmail.com>
2017-11-27 12:38:43 +0545

    Added modified LeNet with Leaky Relu

--- architecture.py
+++ architecture.py
@@ -3,6 +3,10 @@ from tensorflow.contrib.layers import flatten
 from parameters_nn import *
 
 
+def leaky_relu(x, alpha):
+    return tf.nn.relu(x) - alpha * tf.nn.relu(-x)
+
+
 def LeNet(x):
     # Layer 1: Convolutional. Input = 32x32xDepth. Output = 28x28x6.
     conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, IN_DEPTH, 6), mean=MEAN, stddev=STD_DEV))
@@ -22,6 +26,56 @@ def LeNet(x):
 
     # Activation.
     conv2 = tf.nn.relu(conv2)
+
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Flatten. Input = 5x5x16. Output = 400.
+    fc0 = flatten(conv2)
+
+    # Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=MEAN, stddev=STD_DEV))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
+
+    # Activation.
+    fc1 = tf.nn.relu(fc1)
+
+    # Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=MEAN, stddev=STD_DEV))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1, fc2_W) + fc2_b
+
+    #  Activation.
+    fc2 = tf.nn.relu(fc2)
+
+    # Layer 5: Fully Connected. Input = 84. Output = Number of levels.
+    fc3_W = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=MEAN, stddev=STD_DEV))
+    fc3_b = tf.Variable(tf.zeros(LABELS))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+
+    return logits
+
+
+def LeNet2(x):
+    # Layer 1: Convolutional. Input = 32x32xDepth. Output = 28x28x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, IN_DEPTH, 6), mean=MEAN, stddev=STD_DEV))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    # Activation.
+    conv1 = leaky_relu(conv1, 0.2)
+
+    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=MEAN, stddev=STD_DEV))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+
+    # Activation.
+    conv2 = leaky_relu(conv2, 0.2)
 
     # Pooling. Input = 10x10x16. Output = 5x5x16.
     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
