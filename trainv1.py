b47b4f54bf3fa5aa33c99bc45e0256d39e79dced
Sulabh Shrestha <sulabh.shr@gmail.com>
2017-11-27 17:17:13 +0545

    Added exception handling for early manual stopping

--- trainv1.py
+++ trainv1.py
@@ -8,6 +8,20 @@ from matplotlib import pyplot as plt
 import os
 
 
+def train_status(start_time, epochs_trained, loss):
+    # Calculating total time
+    total_time = time() - start_time
+    print("Training Time: {:.2f} sec".format(total_time))
+    print("Training Time per Epoch: {:.2f} sec".format(total_time / epochs_trained))
+
+    os.system('spd-say -p +35 -r -30 "The training has finished. Thank you"')
+    plt.plot(list(range(1, len(loss) + 1)), loss, 'ro', linestyle='dashed', dash_joinstyle='round')
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.axis([0, epochs_trained, 0, 1])
+    plt.show()
+
+
 def evaluate(x_data, y_data, logits, one_hot_y, x, y):
     correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
     accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -43,74 +57,68 @@ def train(x_train, y_train, x_valid, y_valid, epochs, batch_size, tolerance=0.01
     loss = []                       # Loss data for plotting
     patience_count = 0              # For patience
 
-    # Starting the session
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        num_train = len(x_train)
-        print("Started training session")
+    try:
+        # Starting the session
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            num_train = len(x_train)
+            print("Started training session")
 
-        # Iterating over each Epoch
-        for i in range(epochs):
-            print("\nEpoch: {}".format(i + 1))
+            # Iterating over each Epoch
+            for i in range(epochs):
+                print("\nEpoch: {}".format(i + 1))
 
-            epoch_start = time()            # Timer to measure epoch time elapsed
-            x_train, y_train = shuffle(x_train, y_train)
+                epoch_start = time()            # Timer to measure epoch time elapsed
+                x_train, y_train = shuffle(x_train, y_train)
 
-            # Iterating over the data in Batch size
-            for offset in range(0, num_train, batch_size):
-                end = offset + BATCH_SIZE
-                batch_x, batch_y = x_train[offset:end], y_train[offset:end]
-                sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+                # Iterating over the data in Batch size
+                for offset in range(0, num_train, batch_size):
+                    end = offset + BATCH_SIZE
+                    batch_x, batch_y = x_train[offset:end], y_train[offset:end]
+                    sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
 
-            print("Epoch Training Time: {:.2f} sec".format(time()-epoch_start))
+                print("Epoch Training Time: {:.2f} sec".format(time()-epoch_start))
 
-            # Calculating accuracy on validation dataset
-            validation_accuracy = evaluate(x_valid, y_valid, logits, one_hot_y, x, y)
+                # Calculating accuracy on validation dataset
+                validation_accuracy = evaluate(x_valid, y_valid, logits, one_hot_y, x, y)
 
-            loss.append(1-validation_accuracy)          # Adding loss to list for plotting
-            epochs_trained = i + 1                      # Used for calculating time per epoch
+                loss.append(1-validation_accuracy)          # Adding loss to list for plotting
+                epochs_trained = i + 1                      # Used for calculating time per epoch
 
-            print("Validation Accuracy = {:.2f} %".format(validation_accuracy*100))
+                print("Validation Accuracy = {:.2f} %".format(validation_accuracy*100))
 
-            # Checking model improvement
-            if validation_accuracy - prev_best_accuracy < -tolerance and patience_count >= patience:
-                print("\nModel Accuracy degraded below Tolerance level......")
-                print("\nBest Model Accuracy = {:.2f} %".format(prev_best_accuracy*100))
-                break
+                # Checking model improvement
+                if validation_accuracy - prev_best_accuracy < -tolerance and patience_count >= patience:
+                    print("\nModel Accuracy degraded below Tolerance level......")
+                    print("\nBest Model Accuracy = {:.2f} %".format(prev_best_accuracy*100))
+                    break
 
-            # Checking if current accuracy is better than best recorded previously
-            if validation_accuracy > prev_best_accuracy:
-                prev_best_accuracy = validation_accuracy
-                patience_count = 0
-                print("Resetting patience count")
+                # Checking if current accuracy is better than best recorded previously
+                if validation_accuracy > prev_best_accuracy:
+                    prev_best_accuracy = validation_accuracy
+                    patience_count = 0
+                    print("Resetting patience count")
 
-                # Save if better accuracy when save_best True and continue to next iteration
-                if save_best:
-                    saver.save(sess, "models/LeNet E" + str(i) + " ACC {:.4f}".format(validation_accuracy))
-                    continue
+                    # Save if better accuracy when save_best True and continue to next iteration
+                    if save_best:
+                        saver.save(sess, "models/LeNet E" + str(i) + " ACC {:.4f}".format(validation_accuracy))
+                        continue
 
-            patience_count += 1
-            print("Increasing patience count to ", patience_count)
-            # Save the model
-            saver.save(sess, "models/LeNet E" + str(i) + " ACC {:.4f}".format(validation_accuracy))
+                patience_count += 1
+                print("Increasing patience count to ", patience_count)
+                # Save the model
+                saver.save(sess, "models/LeNet E" + str(i) + " ACC {:.4f}".format(validation_accuracy))
+        # Call the function that gives status report
+        train_status(start_time, epochs_trained, loss)
 
-    # Calculating total time
-    total_time = time() - start_time
-    print("Training Time: {:.2f} sec".format(total_time))
-    print("Training Time per Epoch: {:.2f} sec".format(total_time/epochs_trained))
-
-    os.system('spd-say -p +35 -r -30 "The training has finished. Thank you"')
-    plt.plot(list(range(1, len(loss)+1)), loss, 'ro', linestyle='dashed', dash_joinstyle='round')
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.axis([0, epochs_trained, 0, 1])
-    plt.show()
+    except (KeyboardInterrupt, SystemExit) as e:
+        print("EXCEPTION DETECTED!!!!")
+        train_status(start_time, epochs_trained, loss)
 
 
 if __name__ == '__main__':
     x_train, y_train, x_valid, y_valid, x_test, y_test = load_data(TRAIN_PICKLE, VALID_PICKLE, TEST_PICKLE)
     train(x_train, y_train, x_valid, y_valid, EPOCHS, BATCH_SIZE, TOLERANCE, PATIENCE)
-
     # a = normalize(x_train[0:5])
     # print(a)
     # print(type(a[0]))
